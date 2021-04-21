cbab6d0f3141927e944c3e3c287b6e11e10b1e50
Haoran Zhang <hzhang9@testium.cs.uoregon.edu>
2021-04-21 15:46:57 -0700

    Update Dockerfile,add 403.html and 404.html into templates, add .gitigore and  complete app.py

--- web/app.py
+++ web/app.py
@@ -1,14 +1,49 @@
 """
-John Doe's Flask API.
+Haoran Zhang's Flask API.
 """
 
-from flask import Flask
-
+from flask import Flask,render_template,abort
+import os
+import configparser
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+config=configparser.ConfigParser()
+config.read("./credentials.ini")#read credentials.ini
+global PORT #set PORT as global
+PORT=config["DEFAULT"]["PORT"] #get port from DEFAULT of credentials.ini
+global DOCROOT #set DOCROOT as global
+DOCROOT=config["DEFAULT"]["DOCROOT"] #get port from DEFAULT of credentials.ini
+global DEBUG
+DEBUG= config["DEFAULT"]["DEBUG"] #get debug from DEFAULT of credentials.ini
+global HOST
+HOST= config["DEFAULT"]["HOST"] #get debug from DEFAULT of credentials.ini
+
+@app.route("/<path:p>")
+def send(p):
+    p= f'{DOCROOT}{p}'#add doc root in front of path
+    if "//" in p or ".." in p or "~" in p:
+    #if // or .. or ~ in path,call abort to 403
+        abort(403)
+    else:
+        if os.path.isfile(p):
+        #if target file exist in the path,send success and content of the file
+            f=open(p,'r')
+            return f.read(),200
+        else:
+        #else,send 404
+            abort(404)
+
+@app.errorhandler(404)
+def error_404(e):
+    #send content in 404.html when send() call abort(404)
+    return render_template('404.html'),404
+
+@app.errorhandler(403)
+def error_403(e):
+    #send content in 403.html when send() call abort(404)
+    return render_template('403.html'),403
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    #DEBUG,HOST and PORT get from credentials.ini
+    app.run(debug=DEBUG, host=HOST,port=PORT)
+
